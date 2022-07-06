38e46cecc957b91dffb1d05769a067183f1cc23c
Ajay Kc <99326419+jtestkc@users.noreply.github.com>
2022-07-06 17:23:48 +0530

    Update question11-20.py

--- question11-20.py
+++ question11-20.py
@@ -1,44 +1,33 @@
-11. Write a Python program to print the documents (syntax, description etc.) of Python
- built-in function(s).
-Sample function : abs()
-Expected Result :
-abs(number) -> number
-Return the absolute value of the argument
+#11. Write a Python program to print the documents (syntax, description etc.) of Python
+# built-in function(s).
+# Sample function : abs()
+# Expected Result :
+# abs(number) -> number
+#Return the absolute value of the argument
 
-code : 
+#code : 
 
 print(abs.__doc__)
 
 
-12. Write a Python program to print the calendar of a given month and year.
-Note : Use 'calendar' module.
+#12. Write a Python program to print the calendar of a given month and year.
+# Note : Use 'calendar' module.
 
-code : import calendar
+#code : 
+
+import calendar
 y = int(input("Input the year : "))
 m = int(input("Input the month : "))
 print(calendar.month(y, m))
 
-13. Write a Python program to print the following 'here document'. 
-Sample string :
-a string that you "don't" have to escape
-This
-is a ....... multi-line
-heredoc string --------> example
+#13. Write a Python program to print the following 'here document'. 
+#Sample string :
+#a string that you "don't" have to escape
+#This
+#is a ....... multi-line
+#heredoc string --------> example
 
-code :
-
- print("""
-a string that you "don't" have to escape
-This
-is a  ....... multi-line
-heredoc string --------> example
-""")
-
-14. Write a Python program to calculate number of days between two dates.
-Sample dates : (2014, 7, 2), (2014, 7, 11)
-Expected output : 9 days
-
-code : 
+#code :
 
 print("""
 a string that you "don't" have to escape
@@ -47,11 +36,26 @@ is a  ....... multi-line
 heredoc string --------> example
 """)
 
+#14. Write a Python program to calculate number of days between two dates.
+#Sample dates : (2014, 7, 2), (2014, 7, 11)
+#Expected output : 9 days
 
-15. Write a Python program to get the volume of a sphere with radius 6.
+#code : 
+
+from datetime import date
+
+f_date = date(2019, 7, 2)
+l_date = date(2016, 7, 11)
+
+difference = l_date - f_date
+
+print(abs(difference.days))
 
 
-code : 
+#15. Write a Python program to get the volume of a sphere with radius 6.
+
+
+#code : 
 
 
 from django.shortcuts import redirect
@@ -66,10 +70,12 @@ volume = 4/3*pi*radius*radius*radius
 print("Sphere Volume is : ", volume)
 
 
-16. Write a Python program to get the difference between a given number and 17, if the number
- is greater than 17 return double the absolute difference. 
+#16. Write a Python program to get the difference between a given number and 17, if the number
+# is greater than 17 return double the absolute difference. 
 
-code : no = int(input("Enter a number: "))
+#code : 
+
+no = int(input("Enter a number: "))
 
 if no < 17:
     print("Difference is: ",17-no)
@@ -80,10 +86,10 @@ else:
     print("Given no is greater then 17 hence double of its absoulte difference is " , 2*b)
 
 
-17. Write a Python program to test whether a number is within 100 of 1000 or 2000. 
+#17. Write a Python program to test whether a number is within 100 of 1000 or 2000. 
 
 
-code : 
+#code : 
 
 no = int(input("Enter a no. "))
 
@@ -95,12 +101,12 @@ else:
     print("Given is not within 100 of 1000 or 2000 ")
 
 
-18. Write a Python program to calculate the sum of three given numbers, if the values are equal
- then return three times of their sum. 
+#18. Write a Python program to calculate the sum of three given numbers, if the values are equal
+# then return three times of their sum. 
 
-code :
+#code :
 
- a = float(input("Enter First no. "))
+a = float(input("Enter First no. "))
 b = float(input("Enter Second no. "))
 c = float(input("Enter Third no. "))
 
@@ -112,9 +118,9 @@ else:
     print("Sum is ", a+b+c)
 
 
-19. Write a Python program to get a new string from a given string where "Is" has been added to
- the front. If the given string already begins with "Is" then return the string unchanged. 
-code : 
+#19. Write a Python program to get a new string from a given string where "Is" has been added to
+# the front. If the given string already begins with "Is" then return the string unchanged. 
+#code : 
 
 string = str(input("Enter a string : "))
 if (len(string) >=2 and string[:2] == 'Is'):
@@ -122,9 +128,9 @@ if (len(string) >=2 and string[:2] == 'Is'):
 else:
     print("Is"+string)
 
-20. Write a Python program to get a string which is n (non-negative integer) copies of a given string. 
+##20. Write a Python program to get a string which is n (non-negative integer) copies of a given string. 
 
-code : 
+#code : 
 
 string = str(input("Enter a string : "))
 rep = int(input("Enter no of rep: "))
